0db7159d21de2fa659a78c7266266c9784c5ea65
Miia Nyyssönen <mi.nyyssonen@gmail.com>
2023-05-29 10:50:34 +0300

    feat: created graphql types in a schema

--- server/src/schemas/schema.ts
+++ server/src/schemas/schema.ts
@@ -1,10 +1,56 @@
 export const typeDefs = `#graphql
   type Category {
-    id: Int
-    name: String
+    id: Int!
+    name: String!
+  }
+
+  "Category Question Count Lookup: Returns the number of questions in the database, in a specific category."
+  type CategoryQuestionCount {
+    "Total count of easy questions"
+  total_easy_question_count: Int
+  "Total count of medium questions"
+  total_hard_question_count: Int
+  "Total count of hard questions"
+  total_medium_question_count: Int
+  "Total count of all of the questions"
+  total_question_count: Int
+}
+
+  type CategoryQuestionsFromId {
+    category_id: Int
+    category_question_count: CategoryQuestionCount
+  }
+
+  type QuizQuestion {
+    category: String
+    correct_answer: String
+    difficulty: String
+    incorrect_answers: [String]
+    question: String
+    type: String
+  }
+
+  input QuestionRequestArgs {
+    amount: Int
+    category: Int
+    difficulty: Difficulty
+    type: QuestionType
+  }
+
+  enum Difficulty {
+    EASY
+    MEDIUM
+    HARD
+  }
+
+  enum QuestionType {
+    MULTIPLE
+    BOOLEAN
   }
 
   type Query {
-    getallCategories: [Category]
+    getAllCategories: [Category!]!
+    getQuestionCountFromCategory(id: Int!): CategoryQuestionCount
+    getRandomQuizQuestions(args: QuestionRequestArgs): [QuizQuestion]
   }
 `;
