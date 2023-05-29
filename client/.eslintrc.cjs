a8ad4dfbd12ac1377928358bc93a8a574d8fc9a8
Miia Nyyssönen <mi.nyyssonen@gmail.com>
2023-05-29 19:46:52 +0300

    refactor: removed boilerplate code

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,15 +1,40 @@
-/* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
-
 module.exports = {
-  root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    "plugin:vue/vue3-essential",
+    "plugin:vue/vue3-essential",
+    "@vue/eslint-config-typescript",
+    "@vue/eslint-config-prettier/skip-formatting",
+    "prettier"
   ],
+  overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest'
+    ecmaVersion: "latest",
+    sourceType: "module",
+    parser: "@typescript-eslint/parser"
+  },
+  plugins: [ "vue" ],
+  rules: {
+    "vue/script-indent": [ "error", 2, { baseIndent: 1 } ],
+    "vue/no-unused-components": "warn",
+    "vue/no-unused-vars": "warn",
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "warn",
+    "@typescript-eslint/type-annotation-spacing": "error",
+    "@typescript-eslint/space-before-function-paren": [ "error", "always" ],
+    // to enforce using type for object type definitions, can be type or interface
+    "@typescript-eslint/consistent-type-definitions": [ "warn", "type" ],
+    "array-bracket-spacing": [ "error", "always", { arraysInArrays: false } ],
+    "space-in-parens": [ "error", "always" ],
+    quotes: [ "error", "double" ],
+    semi: [ "error", "always" ],
+    "comma-spacing": [ "error", { before: false, after: true } ],
+    "object-curly-spacing": [ "error", "always" ],
+    "template-curly-spacing": [ "error", "always" ],
+    "max-len": [ "warn", { code: 120 } ]
   }
-}
+};
+
