ed95c4cbfb84dd8a2ef5df6782d7ce3924674890
Miia Nyyssönen <mi.nyyssonen@gmail.com>
2023-05-29 10:50:51 +0300

    feat: created resolvers

--- server/src/resolvers.ts
+++ server/src/resolvers.ts
@@ -1,9 +1,19 @@
 import { ContextValue } from "./types/context";
+import { QuestionRequestArgs } from "./types/quizQuestions";
 
 const resolvers = {
   Query: {
-    getallCategories: async ( _: any, __: any, { dataSources }: ContextValue ) => {
+    getAllCategories: async ( _: any, __: any, { dataSources }: ContextValue ) => {
       return dataSources.triviaApi.getCategories();
+    },
+    getQuestionCountFromCategory: async ( _: any, { id }: {id: number}, { dataSources }: ContextValue   ) => {
+      console.log( "id", id );
+      return await dataSources.triviaApi.getCategoryQuestionCount( id );
+    },
+    getRandomQuizQuestions: async ( _: any, questionArgs: QuestionRequestArgs, { dataSources }: ContextValue ) => {
+
+      console.log( "args", questionArgs );
+      return dataSources.triviaApi.getQuizQuestions( questionArgs );
     },
   },
 };
