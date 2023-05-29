17ac05ccabcc3966a3b9d32cf2bfb0d850eb8683
Miia Nyyssönen <mi.nyyssonen@gmail.com>
2023-05-29 10:50:16 +0300

    feat; added typescript types

--- server/src/types/categories.ts
+++ server/src/types/categories.ts
@@ -1,8 +1,20 @@
 export type Category = {
     id: number;
     name: string;
-  }
-  
+}
+
 export type CategoryResponse = {
-    trivia_categories: Category[];
-  }
+  trivia_categories: Category[];
+}
+
+export type CategoryQuestionCount = {
+  total_easy_question_count: number;
+  total_hard_question_count: number;
+  total_medium_question_count: number;
+  total_question_count: number;
+}
+
+export type CategoryQuestionsFromId = {
+  category_id: number;
+  category_question_count: CategoryQuestionCount;
+}
---
+++ server/src/types/quizQuestions.ts
@@ -0,0 +1,29 @@
+
+export type QuizQuestion = {
+  category: string;
+  correct_answer: string;
+  difficulty: string;
+  incorrect_answers: string[];
+  question: string;
+  type: string;
+}
+
+export type QuestionRequestArgs = {
+  args: {
+    amount?: number;
+    category?: number;
+    difficulty?: Difficulty;
+    type?: QuestionType;
+  }
+}
+
+  enum Difficulty {
+    EASY = "easy",
+    MEDIUM = "medium",
+    HARD = "hard",
+  }
+
+  enum QuestionType {
+    MULTIPLE = "multiple",
+    BOOLEAN = "boolean",
+  }
