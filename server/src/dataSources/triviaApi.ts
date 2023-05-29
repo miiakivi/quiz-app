f1f20a72f8e11317eb7aaff936abe13edd709e2a
Miia Nyyssönen <mi.nyyssonen@gmail.com>
2023-05-29 10:51:54 +0300

    update: updated and created more methods in TriviaApi

--- server/src/dataSources/triviaApi.ts
+++ server/src/dataSources/triviaApi.ts
@@ -1,20 +1,37 @@
 import { RESTDataSource } from "@apollo/datasource-rest";
-import { CategoryResponse } from "../types/categories";
+
+import { GraphQLError } from "graphql";
+
+import { CategoryQuestionCount, CategoryResponse } from "../types/categories";
+import { QuestionRequestArgs, QuizQuestion } from "../types/quizQuestions";
 
 export default class TriviaAPI extends RESTDataSource {
   override baseURL = "https://opentdb.com/";
-  test = "test";
-  anotrher = "test ";
-  te = [ 1, 2, 3 ];
-  
+
   async getCategories (): Promise<CategoryResponse> {
     const response = await this.get( "api_category.php" );
     return response.trivia_categories;
   }
-  
-  async getCategoryQuestionCount (): Promise<CategoryResponse> {
-    const response = await this.get( "api_category.php" );
-    return response.trivia_categories;
+
+  async getCategoryQuestionCount ( id: number ): Promise<CategoryQuestionCount> {
+    const response = await this.get( `api_count.php?category=${ id }` );
+    return response.category_question_count;
   }
-  
+
+  async getQuizQuestions ( { args: { amount = 10, category, difficulty, type } }: QuestionRequestArgs ): Promise<QuizQuestion[]> {
+    if ( amount < 1 || amount > 50 ) {
+      throw new GraphQLError( "Invalid value. Amount must be between 1 and 50" );
+    }
+
+    if ( category < 9 || category > 32  ) {
+      throw new GraphQLError( "Invalid value. category id must be between 9 and 32" );
+    }
+
+    const url = `api.php?amount=${ encodeURIComponent( amount ) }${ category ? `&category=${ encodeURIComponent( category ) }` : "" }${ difficulty ? `&difficulty=${ encodeURIComponent( difficulty ) }` : "" }${ type ? `&type=${ encodeURIComponent( type ) }` : "" }`;
+
+    const response = await this.get( url );
+    return response.results;
+  }
+
+
 }
