d29496c577fa1c2de5598b5f31d3a77a1a473f9f
Miia Nyyssönen <mi.nyyssonen@gmail.com>
2023-06-07 14:20:15 +0300

    feat: added option type and put first options to own file

---
+++ client/src/data/options.ts
@@ -0,0 +1,5 @@
+export const answerLabels = [ "a", "b", "c", "d" ];
+
+export const answerOptions = [ "Suprise me",  "Select game level", "Select everything" ];
+
+export const difficultyOptions = [ "Easy", "Medium", "Hard" ];
