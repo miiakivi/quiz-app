d63454bc5d881490e54916f2b9d4a2ab0be81d7c
Miia Nyyssönen <mi.nyyssonen@gmail.com>
2023-06-09 16:30:57 +0300

    feat: created reusable Button component

---
+++ client/src/components/ButtonComponent.vue
@@ -0,0 +1,35 @@
+<template>
+  <button @click="clickEvent()" :class="getButtonClassNames()">
+    {{ props.label }}
+    <IconComponent :icon-name="icon ? icon : 'double-arrow'"/>
+  </button>
+</template>
+
+<script setup lang="ts">
+import IconComponent from "./IconComponent.vue";
+
+type Props = {
+  label: string;
+  disabled: boolean;
+  icon?: string;
+  classNames?: string;
+}
+
+function getButtonClassNames (): string {
+  const classes = [ "btn" ];
+
+  if ( props.classNames ) classes.push( props.classNames );
+  if ( props.disabled ) classes.push( "disabled" );
+
+  return classes.join( " " );
+}
+
+const props = defineProps<Props>();
+const emits = defineEmits( [ "handleButtonClick" ] );
+
+function clickEvent (): void {
+  console.log( "inside button" );
+  emits( "handleButtonClick" );
+}
+
+</script>
