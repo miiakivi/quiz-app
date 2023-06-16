a6b788dc7a222c349aaadd4629ce19cef6bd21f8
Miia Nyyssönen <mi.nyyssonen@gmail.com>
2023-06-16 14:04:27 +0300

    feat: added icons for answered questions

---
+++ client/src/icons/question-mark--light.vue
@@ -0,0 +1,8 @@
+<template>
+  <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="11" cy="11" r="8.25" stroke="#424F91" stroke-opacity="0.41" stroke-width="2"/>
+    <circle cx="11" cy="16.5" r="0.458333" fill="#424F91" fill-opacity="0.41" stroke="#424F91" stroke-opacity="0.41"/>
+    <path d="M11 14V12.8978C11 11.9699 11.6103 11.1525 12.5 10.8889V10.8889C13.3897 10.6253 14 9.80789 14 8.87999V8.54452C14 7.13922 12.8608 6 11.4555 6H10.6667C9.19391 6 8 7.19391 8 8.66667V8.66667" stroke="#424F91" stroke-opacity="0.41" stroke-width="2" stroke-linecap="round"/>
+  </svg>
+
+</template>
---
+++ client/src/icons/question-mark.vue
@@ -0,0 +1,7 @@
+<template>
+  <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="11" cy="11" r="8.25" stroke="#7C90F7" stroke-width="2"/>
+    <circle cx="11" cy="16.5" r="0.458333" fill="#7C90F7" stroke="#7C90F7"/>
+    <path d="M11 14V12.8978C11 11.9699 11.6103 11.1525 12.5 10.8889V10.8889C13.3897 10.6253 14 9.80789 14 8.87999V8.54452C14 7.13922 12.8608 6 11.4555 6H10.6667C9.19391 6 8 7.19391 8 8.66667V8.66667" stroke="#7C90F7" stroke-width="2" stroke-linecap="round"/>
+  </svg>
+</template>
