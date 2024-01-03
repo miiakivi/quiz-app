c97c8715b3788f567c6ed2d5799130f4a33090af
Miia Nyyssönen <mi.nyyssonen@gmail.com>
2024-01-03 13:01:02 +0200

    update: I think i refactored and updated something. Not sure

--- client/src/components/QuizPoints.vue
+++ client/src/components/QuizPoints.vue
@@ -1,25 +1,20 @@
 <template>
   <div v-if="props.answeredQuestions" class="points-container">
-    <transition-group name="fade">
+    <TransitionGroup name="fade">
       <div class="flex" v-for="(answer, index) in props.answeredQuestions" :key="index">
-        <transition
-          name="fade"
-          mode="out-in"
-          :key="getIconKey(answer, index)"
-          @before-enter="beforeEnter"
-          @after-enter="afterEnter"
-        >
-          <IconComponent :icon-name="getIconName(answer, index)" />
-        </transition>
+        <Transition name="fade" mode="out-in">
+          <IconComponent icon-name="check-mark" v-if="answer === 'correct'"/>
+          <IconComponent icon-name="close-mark" v-else-if="answer === 'wrong'"/>
+          <IconComponent icon-name="question-mark" v-else-if="index === props.questionIndex"/>
+          <IconComponent icon-name="question-mark--light" v-else/>
+        </Transition>
       </div>
-    </transition-group>
+    </TransitionGroup>
   </div>
 </template>
 
 <script setup lang="ts">
 import IconComponent from "./IconComponent.vue";
-
-//import { computed, onBeforeEnter, onAfterEnter, defineProps } from "vue";
 
 type Props = {
   answeredQuestions: string[] | undefined;
@@ -28,38 +23,6 @@ type Props = {
 
 const props = defineProps<Props>();
 
-const getIconName = ( answer: string, index: number ): string => {
-  if ( answer === "correct" ) {
-    return "check-mark";
-  } else if ( answer === "wrong" ) {
-    return "close-mark";
-  } else if ( index === props.questionIndex ) {
-    return "question-mark";
-  } else {
-    return "question-mark--light";
-  }
-};
-
-const getIconKey = ( answer: string, index: number ): string => {
-  return `${ answer }_${ index }`;
-};
-
-const beforeEnter = ( el: HTMLElement ): void => {
-  el.style.opacity = "0"; // Set initial opacity to 0 before entering
-};
-
-const afterEnter = ( el: HTMLElement ): void => {
-  el.style.opacity = "1"; // Set final opacity to 1 after entering
-};
-
-const onBeforeLeave = ( el: HTMLElement ): void => {
-  el.style.opacity = "1"; // Set initial opacity to 0 before entering
-};
-
-const onAfterLeave = ( el: HTMLElement ): void => {
-  el.style.opacity = "0"; // Set final opacity to 1 after entering
-};
-
 </script>
 
 <style scoped lang="less">
@@ -67,26 +30,20 @@ const onAfterLeave = ( el: HTMLElement ): void => {
 .points-container {
   display: flex;
   gap: 0.5rem;
-
 }
 .fade-enter-from {
   opacity: 0;
   transform: translateY(100%);
-  border: 1px solid rgb(255, 92, 47);
-
 }
 
 .fade-leave-to {
   opacity: 0;
   transform: translateY(-100%);
-  border: 1px solid rgb(134, 47, 255);
-
 }
 
 .fade-enter-active,
 .fade-leave-active {
   transition: opacity 0.3s, transform 0.3s;
-  border: 1px solid greenyellow;
 }
 
 </style>
