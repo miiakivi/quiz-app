a6fde005f3c8aaa51842f3ff5e9b5303948b2e64
Miia Nyyssönen <mi.nyyssonen@gmail.com>
2024-01-03 12:59:49 +0200

    style: changed game settings text

--- client/src/data/options.ts
+++ client/src/data/options.ts
@@ -2,11 +2,20 @@ import type { QuestionType } from "../types/QuestionType";
 
 export const gameSettings: QuestionType[] = [
   {
-    question: "Select your game mode",
-    options:  [ "Suprise me",  "Select game level", "Select everything" ]
+    question: "Think you've got what it takes? First decide how you would like to play",
+    options:  [ "Let the fate decide!",  "Customize your challenge" ]
   },
   /*{
     question: "How brave are you?",
     options: [ "Easy", "Medium", "Hard" ]
-  },*/
+  },
+  {
+    question: "How many quiestions you want?",
+    options: [ "10", "20", "35", "50" ]
+  },
+  {
+    question: "Select question category?",
+    options: [ "Easy", "Medium", "Hard" ]
+  },
+  */
 ];
