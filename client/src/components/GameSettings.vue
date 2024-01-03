d515315fbe7b48e64f705d52da8dbb7a343c58a6
Miia Nyyssönen <mi.nyyssonen@gmail.com>
2024-01-03 15:47:52 +0200

    feat: new component that gets user to select their own game mode

---
+++ client/src/components/GameSettings.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <CustomSelect
+      :options="quizDifficulty"
+      :default="'select'"
+      @input="inputSelected"
+    />
+    <CustomSelect
+      :options="quizDifficulty"
+      :default="'select'"
+      @input="inputSelected"
+    />
+    <CustomSelect
+      :options="quizDifficulty"
+      :default="'select'"
+      @input="inputSelected"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { watch } from "vue";
+
+import { useQuery } from "@vue/apollo-composable";
+
+
+import quizDifficulty from "../data/quizz-difficulty";
+import CustomSelect from "./CustomSelect.vue";
+
+import { GET_QUIZ_CATEGORIES } from "../graphql/query";
+
+const { loading, error, result } = useQuery( GET_QUIZ_CATEGORIES );
+
+watch( result, () => {
+  console.log( "category results", result.value );
+} );
+
+console.log( quizDifficulty );
+
+const inputSelected = ( input: string ): void => {
+  console.log( "selected input:", input );
+};
+
+</script>../data/quizz-difficulty
