00c168748d2f71ba32743bda341245a7aa13513c
Miia Nyyssönen <mi.nyyssonen@gmail.com>
2024-01-10 15:47:08 +0200

    style: removed comments

--- client/src/data/options.ts
+++ client/src/data/options.ts
@@ -5,17 +5,4 @@ export const gameSettings: QuestionType[] = [
     question: "Think you've got what it takes? First decide how you would like to play",
     options:  [ "Let the fate decide!",  "Customize your challenge" ]
   },
-  /*{
-    question: "How brave are you?",
-    options: [ "Easy", "Medium", "Hard" ]
-  },
-  {
-    question: "How many quiestions you want?",
-    options: [ "10", "20", "35", "50" ]
-  },
-  {
-    question: "Select question category?",
-    options: [ "Easy", "Medium", "Hard" ]
-  },
-  */
 ];
